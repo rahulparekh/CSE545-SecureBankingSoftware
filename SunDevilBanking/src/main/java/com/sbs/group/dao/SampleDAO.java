07574a226665fb9f76555ee4a654b7f14fd1553a
SUNILS <venky.sunil@gmail.com>
2015-10-10 20:02:05 -0700

    dao classes folder

---
+++ SunDevilBanking/src/main/java/com/sbs/group/dao/SampleDAO.java
@@ -0,0 +1,11 @@
+package com.sbs.group.dao;
+
+/**
+ * Created by SUNILS on 10/10/2015.
+ */
+public class SampleDAO {
+    private static final Log log = LogFactory.getLog(UserHome.class);
+
+    private SessionFactory sessionFactory;
+
+}
