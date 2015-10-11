2e6ee60d2e9eeb75238b9846692c0a7c39c0cf36
SUNILS <venky.sunil@gmail.com>
2015-10-10 20:52:44 -0700

    Folder Structure commit

---
+++ SunDevilBanking/src/main/java/com/sbs/group11/model/SampleModel.java
@@ -0,0 +1,7 @@
+package com.sbs.group11.model;
+
+/**
+ * Created by SUNILS on 10/10/2015.
+ */
+public class SampleModel {
+}
---
+++ SunDevilBanking/src/main/java/com/sbs/group11/service/SampleService.java
@@ -0,0 +1,8 @@
+package com.sbs.group11.service;
+
+/**
+ * Created by SUNILS on 10/10/2015.
+ */
+public class SampleService {
+    //Sample Service Class
+}
