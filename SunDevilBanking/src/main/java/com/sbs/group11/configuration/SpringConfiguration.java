b7bf121b5df7ea1f52eb0d61fc920b29ebc81b9b
Rahul Parekh <contact@rahulparekh.in>
2015-10-16 15:11:32 -0700

    javaconfig

---
+++ SunDevilBanking/src/main/java/com/sbs/group11/configuration/SpringConfiguration.java
@@ -0,0 +1,33 @@
+package com.sbs.group11.configuration;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.ViewResolver;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
+import org.springframework.web.servlet.view.JstlView;
+
+@Configuration
+@EnableWebMvc
+@ComponentScan(basePackages = "com.sbs.group11")
+public class SpringConfiguration extends WebMvcConfigurerAdapter {
+	
+	@Bean
+    public ViewResolver viewResolver() {
+        InternalResourceViewResolver viewResolver = new InternalResourceViewResolver();
+        viewResolver.setViewClass(JstlView.class);
+        viewResolver.setPrefix("/WEB-INF/views/");
+        viewResolver.setSuffix(".jsp");
+ 
+        return viewResolver;
+    }
+	
+	@Override
+	public void addResourceHandlers(final ResourceHandlerRegistry registry) {
+	    registry.addResourceHandler("/static/**").addResourceLocations("/static/");
+	}
+
+}
