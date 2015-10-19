7a630cf19032498529fdab5aecf554325f37e738
Rahul Parekh <contact@rahulparekh.in>
2015-10-19 14:26:20 -0700

    Added Role model

---
+++ SunDevilBanking/src/main/java/com/sbs/group11/model/Role.java
@@ -0,0 +1,60 @@
+package com.sbs.group11.model;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "Role")
+public class Role {
+	
+	private Integer userRoleId;
+	private User user;
+	private String role;
+
+	public Role() {
+	}
+
+	public Role(User user, String role) {
+		this.user = user;
+		this.role = role;
+	}
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "UserRoleID", 
+		unique = true, nullable = false)
+	public Integer getUserRoleId() {
+		return this.userRoleId;
+	}
+
+	public void setUserRoleId(Integer userRoleId) {
+		this.userRoleId = userRoleId;
+	}
+
+	@ManyToOne(fetch = FetchType.LAZY)
+	@JoinColumn(name = "CustomerID", nullable = false)
+	public User getUser() {
+		return this.user;
+	}
+
+	public void setUser(User user) {
+		this.user = user;
+	}
+
+	@Column(name = "Role", nullable = false, length = 45)
+	public String getRole() {
+		return this.role;
+	}
+
+	public void setRole(String role) {
+		this.role = role;
+	}
+
+}
